baa1d26b615afcee2f9c79bffd36445b0a1ec77f
onlyliubara <onlyliubara@gmail.com>
2017-12-22 14:27:40 +0800

    修改了animate

--- src/components/Animate.js
+++ src/components/Animate.js
@@ -1,20 +1,24 @@
 import React,  { Component } from 'react'
 
 class Animate extends Component {
-    handleAnim() {
-        var mask1 = document.querySelector('.animate')
-        var mask2 = document.querySelector('.mask2')
-        var mask3 = document.querySelector('.mask1')
-        
-        mask1.classList.add('hide')
-        mask2.classList.add('change')
-        mask3.classList.add('change1')        
+    constructor() {
+        super()
+        this.state={
+            isClicked: false
+        }
     }
+
+    handleAnim() {
+        this.setState({
+            isClicked: true
+        })      
+    }
+
     render() {
         return (
-            <div className="animate">
-                <div className="mask1"></div>
-                <div className="mask2"></div>
+            <div className={`${this.state.isClicked ?` animate hide `:` animate `}`}>
+                <div className={`${this.state.isClicked ?` mask1 change1 `:` mask1 `}`}></div>
+                <div className={`${this.state.isClicked ?` mask2 change `:` mask2 `}`}></div>
                 <div className="open" onClick={this.handleAnim.bind(this)}>拆</div>
             </div>
         )
